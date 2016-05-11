075247b4d477c3dde25ca076bdea395d15173b91
bhbsolutions <bennour.hassen@gmail.com>
2016-05-11 16:07:46 +0200

    updated GRANT_PERMISSION to read jar's from folder

--- JMSSenderConnector/sql/1-permissions.sql
+++ JMSSenderConnector/sql/1-permissions.sql
@@ -1,7 +1,13 @@
+--To change :
+--<USER_SCHEMA>: schema to load into java classes
+--<JMSSenderConnectorLibsFolder> :  physical path to directory where jar's are.
+--<HOSTNAMEorIP> : host name or IP
+--<PORT> : used port for TCP or openwire
 
-CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<ToChange-ORACLE_SCHEMA_USER>', 'SYS:java.lang.reflect.ReflectPermission', '*', '' );
-CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<ToChange-ORACLE_SCHEMA_USER>', 'SYS:java.lang.RuntimePermission', '*', '' );
-CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<ToChange-ORACLE_SCHEMA_USER>', 'SYS:oracle.aurora.security.JServerPermission', '*', '' );
-CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<ToChange-ORACLE_SCHEMA_USER>', 'SYS:java.util.PropertyPermission', '*', 'read,write' );
-CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<ToChange-ORACLE_SCHEMA_USER>', 'SYS:java.net.SocketPermission', '<ToChange-HOSTNAMEorIP>:<ToChange-PORT>', 'accept, connect, listen, resolve');
-call SYS.DBMS_JAVA.GRANT_PERMISSION( '<ToChange-ORACLE_SCHEMA_USER>', 'SYS:java.util.PropertyPermission', 'java.net.preferIPv4Stack', 'write' );
+CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'SYS:java.lang.reflect.ReflectPermission', '*', '' );
+CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'SYS:java.lang.RuntimePermission', '*', '' );
+CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'SYS:oracle.aurora.security.JServerPermission', '*', '' );
+CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'SYS:java.util.PropertyPermission', '*', 'read,write' );
+CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'SYS:java.net.SocketPermission', '<HOSTNAMEorIP>:<PORT>', 'accept, connect, listen, resolve');
+CALL SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'java.io.FilePermission', '<JMSSenderConnectorLibsFolder>/-', 'read,write,delete');
+call SYS.DBMS_JAVA.GRANT_PERMISSION( '<USER_SCHEMA>', 'SYS:java.util.PropertyPermission', 'java.net.preferIPv4Stack', 'write' );
--- JMSSenderConnector/sql/2-load-into-oracle.sql
+++ JMSSenderConnector/sql/2-load-into-oracle.sql
@@ -1,7 +1,7 @@
 --To change :
 --<USER_SCHEMA>: schema to load into java classes
 --<USER/PWD> : user & password for this schema
---<PATH_TO_DIRECTORY> :  physical path to directory where jar's are.
+--<JMSSenderConnectorLibsFolder> :  physical path to directory where jar's are.
 
-call dbms_java.LOADJAVA('<PATH_TO_DIRECTORY>/activemq-all-clean-5.10.0.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* -))" -resolve -genmissing -verbose  -grant <USER_SCHEMA>');
-call dbms_java.LOADJAVA('<PATH_TO_DIRECTORY>/JMSSenderConnector.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* -))" -resolve -verbose  -grant <USER_SCHEMA>');
+call dbms_java.LOADJAVA('<JMSSenderConnectorLibsFolder>/activemq-all-clean-5.10.0.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* -))" -resolve -genmissing -verbose  -grant <USER_SCHEMA>');
+call dbms_java.LOADJAVA('<JMSSenderConnectorLibsFolder>/JMSSenderConnector.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* -))" -resolve -verbose  -grant <USER_SCHEMA>');
