29c413dff0f206d154c37c01dcab3929caa1c608
bhbsolutions <bennour.hassen@gmail.com>
2016-05-11 15:58:43 +0200

    add lib activemq-all-clean-5.10.0.jar

--- JMSSenderConnector/sql/2-load-into-oracle.sql
+++ JMSSenderConnector/sql/2-load-into-oracle.sql
@@ -4,4 +4,4 @@
 --<PATH_TO_DIRECTORY> :  physical path to directory where jar's are.
 
 call dbms_java.LOADJAVA('<PATH_TO_DIRECTORY>/activemq-all-clean-5.10.0.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* -))" -resolve -genmissing -verbose  -grant <USER_SCHEMA>');
-call dbms_java.LOADJAVA('<PATH_TO_DIRECTORY>/JMSSenderConnector.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* SYS)(* -))" -resolve -verbose  -grant <USER_SCHEMA>');
+call dbms_java.LOADJAVA('<PATH_TO_DIRECTORY>/JMSSenderConnector.jar -v -u <USER/PWD> -resolver "((* <USER_SCHEMA>) (* PUBLIC) (* -))" -resolve -verbose  -grant <USER_SCHEMA>');
